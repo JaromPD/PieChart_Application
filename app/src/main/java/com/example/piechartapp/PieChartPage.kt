813f9976b7d3e127d58031d75db044acaf7d76a4
Jarom Diaz <92468206+JaromPD@users.noreply.github.com>
2022-05-25 13:34:21 -0600

    Initial commit

---
+++ app/src/main/java/com/example/piechartapp/PieChartPage.kt
@@ -0,0 +1,11 @@
+package com.example.piechartapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PieChartPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pie_chart_page)
+    }
+}
